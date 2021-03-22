374452070a7af32ddf10f182d1788ffa6a9ea422
yuval5424 <71177869+yuval5424@users.noreply.github.com>
2021-03-22 21:28:32 +0200

    Update searchCommand.py

--- cogs/searchCommand.py
+++ cogs/searchCommand.py
@@ -7,6 +7,10 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
 web = webdriver.Chrome("chromedriver.exe")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.binary_location = GOOGLE_CHROME_PATH
 class searchCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
