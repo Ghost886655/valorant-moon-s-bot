6174efb42baf4946ab712cba71fb1daf9841f793
yuval5424 <71177869+yuval5424@users.noreply.github.com>
2021-03-24 02:26:15 +0200

    Update searchCommand.py

--- cogs/searchCommand.py
+++ cogs/searchCommand.py
@@ -12,6 +12,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-sh-usage')
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+web = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options = chrome_options)
 class searchCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -19,7 +20,6 @@ class searchCommand(commands.Cog):
     async def search(self, ctx, *, player=""):
             if player == "" or "#" not in player:
                 return await ctx.send("שימוש לא נכון בפקודה! שימוש נכון: שחקן search++")
-            web = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options = chrome_options)
             web.get("https://tracker.gg/valorant")
             web.find_element_by_tag_name("input").send_keys(player)
             time.sleep(1)
@@ -42,7 +42,6 @@ class searchCommand(commands.Cog):
             avatar_url = avatar.get_attribute("href")
             stats.set_thumbnail(url=avatar_url)
             await ctx.send(embed=stats)
-            web.close()
 
 def setup(bot):
     bot.add_cog(searchCommand(bot))
