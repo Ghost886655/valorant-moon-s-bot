c8ef12780036b5b228d727f31e62bc3273bd5fd5
yuval5424 <71177869+yuval5424@users.noreply.github.com>
2021-03-24 02:20:16 +0200

    Update searchCommand.py

--- cogs/searchCommand.py
+++ cogs/searchCommand.py
@@ -8,6 +8,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-sh-usage')
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
