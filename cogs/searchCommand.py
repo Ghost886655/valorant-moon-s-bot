cb34a753d2adb161282cf36afe4402f6d6c13961
yuval5424 <71177869+yuval5424@users.noreply.github.com>
2021-03-24 20:40:28 +0200

    Update searchCommand.py

--- cogs/searchCommand.py
+++ cogs/searchCommand.py
@@ -41,6 +41,10 @@ class searchCommand(commands.Cog):
         stats.set_thumbnail(url=avatar_url)
         await ctx.send(embed=stats)
         web.close()
+    @search.error
+    async def search_error(self, ctx, error):
+        if isinstance(error, WebDriverException):
+            pass
 
 def setup(bot):
     bot.add_cog(searchCommand(bot))
