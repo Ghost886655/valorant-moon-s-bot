717eaaa2d0f9b6d93c8eb8b88aa2ad8208a9f14c
yuval5424 <71177869+yuval5424@users.noreply.github.com>
2021-03-24 02:31:01 +0200

    Update searchCommand.py

--- cogs/searchCommand.py
+++ cogs/searchCommand.py
@@ -29,10 +29,11 @@ class searchCommand(commands.Cog):
             except NoSuchElementException:
                 return await ctx.send("משתמש זה לא קיים או שלא מחובר לhttps://tracker.gg/valorant בדוק שנית אם כתבת ללא שגיאות.")
             name, discriminator = player.split("#")
-            web.get(f"https://tracker.gg/valorant/profile/riot/{name}%23{discriminator}/overview?playlist=competitive")
             rank = web.find_element_by_class_name("valorant-highlighted-stat__value").text()
             name = web.find_element_by_class_name("trn-ign__username").text
-            discriminator = web.find_element_by_class_name("trn-ign__discriminator").text
+            descriminator = web.find_element_by_class_name("trn-ign__discriminator").text
+            descriminator = descriminator.replace("#", "")
+            web.get(f"https://tracker.gg/valorant/profile/riot/{name}%23{descriminator}/overview?playlist=competitive")
             KAD = web.find_elements_by_class_name("valorant-highlighted-stat__value")
             KAD = KAD[1].text
             stats = discord.Embed(title="שם:", description=name + discriminator,color=discord.Color.red())
