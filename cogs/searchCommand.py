4513ef6c6372daa93f698151736bda5a76d4d332
yuval5424 <71177869+yuval5424@users.noreply.github.com>
2021-03-25 01:16:35 +0200

    Update searchCommand.py

--- cogs/searchCommand.py
+++ cogs/searchCommand.py
@@ -19,9 +19,9 @@ class searchCommand(commands.Cog):
         self.bot = bot
     @commands.command()
     async def search(self, ctx, *, player=""):
-        await ctx.send("מחפש...")
         if player == "" or "#" not in player:
-            return await ctx.send("שימוש לא נכון בפקודה! שימוש נכון: שחקן search++"        
+            return await ctx.send("שימוש לא נכון בפקודה! שימוש נכון: שחקן search++")
+        await ctx.send("מחפש...")
         web.get("https://tracker.gg/valorant")
         web.find_element_by_tag_name("input").send_keys(player)
         time.sleep(1)
@@ -32,16 +32,24 @@ class searchCommand(commands.Cog):
         except NoSuchElementException:
             return await ctx.send(
                 "משתמש זה לא קיים או שלא מחובר לhttps://tracker.gg/valorant בדוק שנית אם כתבת ללא שגיאות.")
-        print(web.current_url)
+        try:
+            web.find_element_by_class_name("lead")
+            await ctx.send("המשתמש נמצא אך הוא פרטי. הירשם לhttps://tracker.gg/valorant כדי לראות את הסטטים שלו")
+        except NoSuchElementException:
+            pass
         stats_on_page = web.find_elements_by_class_name("valorant-highlighted-stat__value")
         rank = stats_on_page[0].text
         KAD = stats_on_page[1].text
         stats = discord.Embed(title="שם:", description=name + descriminator, color=discord.Color.red())
         stats.add_field(name="ראנק:", value=rank, inline=False)
         stats.add_field(name="KAD:", value=KAD, inline=False)
+        main_gun = web.find_element_by_class_name("weapon__info")
+        main_gun_image = main_gun.find_element_by_tag_name("img").get_attribute("src")
+        stats.add_field(name="נשק אהוב:", description = main_gun.text, value=discord.Color.red())
         avatar = web.find_element_by_tag_name("image")
         avatar_url = avatar.get_attribute("href")
         stats.set_thumbnail(url=avatar_url)
+        stats.set_image(url=main_gun_image)
         await ctx.send(embed=stats)
 
 def setup(bot):
