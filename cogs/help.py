79434c97c7bed613eaa13da81fabdbdf5e1a18b6
yuval5424 <71177869+yuval5424@users.noreply.github.com>
2021-03-29 18:43:58 +0300

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -8,7 +8,7 @@ class Help(commands.Cog):
     @commands.command(aliases=['h'])
     async def help(self, ctx):
         embed = discord.Embed(title="פקודות:",
-                              description="++j -  בשביל להצטרף לקיו\n++l - בשביל לצאת מהקיו\n++teamsize משנה את מספר השחקנים בטים - מספר")
+                              description="++j -  בשביל להצטרף לקיו\n++l - בשביל לצאת מהקיו\n++teamsize משנה את מספר השחקנים בטים - מספר\n++search שחקן")
         embed.set_footer(text="בוט על ידי Quidy")
         await ctx.send(embed=embed)
 
